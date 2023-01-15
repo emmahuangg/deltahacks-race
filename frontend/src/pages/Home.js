6c1851e32e5145569a0e65ff4a1b3854ae5e2f55
Emma Huang <emmahuang.ty@gmail.com>
2023-01-15 02:01:44 -0500

    edited title slide

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -13,7 +13,7 @@ export const Home = () => {
   }
   return (
     <div className="w-screen h-auto pb-20">
-      <div className="w-screen h-screen bg-bottom bg-[url('../public/abstract.jpg')] bg-cover">
+      <div className="w-screen h-600 bg-bottom bg-[url('../public/abstract.jpg')] bg-cover">
         <Navigation />
         <span className='flex justify-center mt-16 px-20'>
           <h1 className="p-0 text-8xl font-extrabold inline-block text-black">Wiki</h1>
