986d3cea42bd98616b95da21b2b65277199d7054
carolynzhang18 <carolyn.zhang18@gmail.com>
2023-01-15 04:15:38 -0500

    python for simplifying
    heehee haha

--- frontend/src/Article.js
+++ frontend/src/Article.js
@@ -5,24 +5,23 @@ export const Article = (props) => {
   const [content, setContent] = useState(props.string);
 
     // function checkString(str) {
-    //     https://api.datamuse.com/words?sp=smartass&md=f&max=1
+    //     https://api.datamuse.com/words?sp=the&md=f&max=1
     // }
 
     function simplify(str) {
       const strArr = str.split(' ');
       strArr.forEach((word) => {
         var start, end;
-        if (!!word.match(/^[.,:!?]/)) {
+        if (!!word.match(/^[.,:!?()]/)) {
           start = word[0];
         }
         word = word.replace(/[^\w\s\']|_/g, "")
-            console.log(word);
             fetch(`https://api.datamuse.com/words?sp=` + word + `&md=f&max=1`)
               .then((response) => response.json())
               .then((data) => {
                 if (data[0]) {
                   console.log(data[0].word + " Original: " + word)
-                  console.log(data[0].score)
+                  console.log(data[0].tags[0][0])
                 } else { 
                   console.log("NOTENOTNEOTNOENTOENOTNTONE" + word)
                 }
