52130c0a194e2999c4187a5aa46b7a4fed48f978
Angela Xu <angela.xu.dev@gmail.com>
2023-01-14 21:12:01 -0500

    added a long search bar
    added a long search bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import Navigation from '../components/Navigation';
+import Searchbar from '../components/Searchbar';
+
+import "../index.css";
 
 export const Home = () => {
     return (
-        <div className="w-screen h-screen bg-bottom bg-[url('../public/assets/images/abstract.jpg')] bg-cover">
+      <>
+        <div className="p-20 w-screen h-screen bg-bottom bg-[url('../public/assets/images/abstract.jpg')] bg-cover">
         <Navigation />
-    </div>
+
+        <div className="p-10 text-5xl font-extrabold">
+        <span class="p-10 bg-clip-text bg-gradient-to-r from-pink-500 to violet-500">
+          WikiLearn </span> 
+
+        <Searchbar />
+        </div>
+        </div>
+
+        
+        
+        
+
+
+        
+      </>
+        
   )
 }
 
